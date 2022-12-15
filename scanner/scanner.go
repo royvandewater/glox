348daea6fed801bc2a59bf4e80868d80b44beff9
Roy van de Water <roy.v.water@gmail.com>
2022-12-15 15:46:56 -0700

    Start implementing the scanner

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -1,11 +1,50 @@
 package scanner
 
+import (
+	"go/token"
+	"unicode/utf8"
+)
+
 func New(source string) *Scanner {
-	return &Scanner{}
+	return &Scanner{
+		source:  source,
+		tokens:  make([]token.Token, 0),
+		start:   0,
+		current: 0,
+		line:    1,
+	}
 }
 
-type Scanner struct{}
+type Scanner struct {
+	source  string
+	tokens  []token.Token
+	start   int
+	current int
+	line    int
+}
 
-func (s Scanner) ScanTokens() []string {
-	return make([]string, 0)
+func (s *Scanner) ScanTokens() []token.Token {
+	for !s.isAtEnd() {
+		s.start = s.current
+		s.scanToken()
+	}
+
+	s.tokens = append(s.tokens, token.EOF)
+
+	return s.tokens
+}
+
+func (s *Scanner) scanToken() {
+
+}
+
+func (s *Scanner) advance() rune {
+	value, width := utf8.DecodeRuneInString(s.source[s.current:])
+
+	s.current = s.current + width
+	return value
+}
+
+func (s *Scanner) isAtEnd() bool {
+	return s.current >= len(s.source)
 }
